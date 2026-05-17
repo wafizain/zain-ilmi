c8e7b23c58e2895bee8bc439e1ad65d023645e4f
wafi <zain.wafim@gmail.com>
2026-05-17 18:28:34 +0700

    update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ const jakarta = Plus_Jakarta_Sans({
 });
 
 export const metadata: Metadata = {
-  title: "Zain Ilmi | Bimbel & Les Privat di Bandung",
+  title: "Zain Ilmi | Bimbingan belajar di Bandung",
 
   description:
     "Bimbel di Bandung untuk Pra-SD & Calistung, SD, SMP, dan SMA dengan suasana belajar yang nyaman dan pendampingan personal.",
